0ac92a804c9f08314883811a379db3f5de9c3b8c
jho951 <jho951@naver.com>
2022-09-04 02:02:28 +0900

    #1 feat 모달 컴포넌트 생성

--- final_project/src/Components/Modal.js
+++ final_project/src/Components/Modal.js
@@ -1,28 +1,36 @@
 import styled from "styled-components";
 
-const Modal = ({ visible, close, children }) => {
+function Modal({ visible, children }) {
   return (
-    <>
-      <ModalWrap visible={visible} onClick={close}></ModalWrap>
+    <div className="Modal">
+      <ModalWrap visible={visible}></ModalWrap>
       <ModalBody visible={visible}>{children}</ModalBody>
-    </>
+    </div>
   );
-};
+}
 export default Modal;
 
 const ModalWrap = styled.div`
-width:100%;
-height:100%;
-display: $((props))=>(props.visible ? 'block' :'none')};
-position: fixed;
-background-color:gray;
+  width: 100%;
+  height: 100%;
+  display: ${(props) => (props.visible ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 99;
+  cursor: pointer;
 `;
 const ModalBody = styled.div`
   display: ${(props) => (props.visible ? "block" : "none")};
   position: fixed;
   top: 50%;
   left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
   background-color: white;
-  width: 500px;
-  height: 500px;
+  border-radius: 10px;
+  width: 330px;
+  height: 550px;
+  padding-left: 10px;
+  z-index: 100;
 `;
