5f9902a24f02ca9d63cf4210f983fc54577a934b
jho951 <jho951@naver.com>
2022-09-04 04:31:51 +0900

    #1 feat 포스트 페이지 카테고리 구성

--- final_project/src/pages/PostPage.js
+++ final_project/src/pages/PostPage.js
@@ -4,12 +4,14 @@ import Toolbar from "@mui/material/Toolbar";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "../Components/Modal";
+import styled from "styled-components";
 
 export default function PostPage() {
   const navigate = useNavigate();
   const [imgState, setImgState] = useState([]);
   const [titleState, setTitleState] = useState("");
   const [contentState, setContentState] = useState("");
+  const [value, setValue] = useState("0");
   const [open, setOpen] = useState(false);
   const AddImage = (e) => {
     const imgSelectList = e.target.files;
@@ -17,6 +19,9 @@ export default function PostPage() {
     for (let i = 0; i < imgSelectList.length; i++) {
       const imgUrl = URL.createObjectURL(imgSelectList[i]);
       imgUploadList.push(imgUrl);
+      if (imgUploadList.length > 5) {
+        imgUploadList = imgUploadList.slice(0, 5);
+      }
     }
     setImgState(imgUploadList);
   };
@@ -32,6 +37,21 @@ export default function PostPage() {
   };
   const OpenModal = () => {
     setOpen(true);
+  };
+  const CloseModal = () => {
+    setOpen(false);
+  };
+  const onChangeHandler = (e) => {
+    const value = e.target;
+    setValue(value);
+    console.log(value);
+    CloseModal();
+  };
+  const ClickHandler = () => {
+    console.log(titleState, contentState, imgState);
+  };
+  const handleDeleteImage = (id) => {
+    setImgState(imgState.filter((_, index) => index !== id));
   };
   return (
     <>
@@ -61,11 +81,13 @@ export default function PostPage() {
             hidden
           />
         </label>
-        {imgState.map((image) => (
+        {imgState.map((image, id) => (
           <img
+            key={id}
             src={image}
-            alt={image}
+            alt={`${image}-${id}`}
             style={{ width: "60px", height: "60px" }}
+            onClick={() => handleDeleteImage(id)}
           />
         ))}
       </div>
@@ -95,29 +117,45 @@ export default function PostPage() {
           placeholder="내용을 입력해주세요."
         />
       </div>
-      <Modal
-        visible={open}
-        closeModal={() => {
-          setOpen(false);
-        }}
-      >
-        <div>디지털기기</div>
-        <div>생활가전</div>
-        <div>생활/주방</div>
-        <div>여성의류/잡화</div>
-        <div>남성의류/잡화</div>
-        <div>뷰티/미용</div>
-        <div>취미/게임</div>
-        <div>도서</div>
-        <div>티켓</div>
-        <button
-          onClick={() => {
-            setOpen(false);
-          }}
-        >
-          돌아가기
-        </button>
+      <Modal visible={open} closeModal={CloseModal}>
+        <Div data-value="1" onClick={onChangeHandler}>
+          디지털기기
+        </Div>
+        <Div data-value="2" onClick={onChangeHandler}>
+          생활가전
+        </Div>
+        <Div data-value="3" onClick={onChangeHandler}>
+          생활/주방
+        </Div>
+        <Div data-value="4" onClick={onChangeHandler}>
+          여성의류/잡화
+        </Div>
+        <Div data-value="5" onClick={onChangeHandler}>
+          남성의류/잡화
+        </Div>
+        <Div data-value="6" onClick={onChangeHandler}>
+          뷰티/미용
+        </Div>
+        <Div data-value="7" onClick={onChangeHandler}>
+          취미/게임
+        </Div>
+        <Div data-value="8" onClick={onChangeHandler}>
+          도서
+        </Div>
+        <Div data-value="9" onClick={onChangeHandler}>
+          티켓
+        </Div>
+        <button onClick={CloseModal}>돌아가기</button>
       </Modal>
+      <button onClick={ClickHandler}>완료</button>
     </>
   );
 }
+const Div = styled.div`
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    background-color: 9e90e6;
+  }
+`;
