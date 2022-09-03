3136c1d7edf468719b36864ea7a329ef78d1998e
jho951 <jho951@naver.com>
2022-09-03 15:05:08 +0900

    #1 feat posting 페이지 라우터 처리

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,26 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import PostPage from "./pages/PostPage";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
 import ErrorPage from "./pages/ErrorPage";
 function App() {
   return (
-
-
-
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="*" element={<ErrorPage />} />
-          {/* <Route path="/login" element={<Login />} />
-        
-        <Route path="/postpage" element={<PostPage />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+
+          <Route path="/postpage" element={<PostPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-
-
   );
 }
 
