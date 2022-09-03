36628c148c6f9189c6a20f7843987d72516e1c82
sangjin <sasangjin@gmail.com>
2022-09-03 21:13:54 +0900

    login페이지 작성중
    메인페이지에서 회원가입 페이지로 한번에 넘어가는 기능 추가 요망

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -3,6 +3,9 @@ import MainPage from "./pages/MainPage";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/LoginPage";
+
+
 function App() {
   return (
 
@@ -13,9 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="*" element={<ErrorPage />} />
-          {/* <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
         
-        <Route path="/postpage" element={<PostPage />} /> */}
+        {/* <Route path="/postpage" element={<PostPage />} /> */}
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- final_project/src/pages/MainPage.js
+++ final_project/src/pages/MainPage.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ScreenSize from "../shared/ScreenSize";
+import { Link, useNavigate } from "react-router-dom"
+
 export default function MainPage() {
+    const navigate = useNavigate()
+    const [register, setRegister] = useState(true)
+    const onRegister = () => {
+        navigate('/login', )
+    }
+    navigate('/login', {})
     return (
         <ScreenSize>
-            <>메인페이지입ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㄹㅇㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ다</>
-            <button>adfs</button>
+            <h1>메인페이지</h1>
+            <Link to="/login">로그인 및 회원가입</Link>
         </ScreenSize>)
 }
