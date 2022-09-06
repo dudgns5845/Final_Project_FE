55a610fdf4a5495166fe4e56839c9f5eb9c929e7
jho951 <jho951@naver.com>
2022-09-06 16:28:18 +0900

    #1 feat 글쓰기 페이지 뷰작업 완성

--- final_project/src/pages/PostPage.js
+++ final_project/src/pages/PostPage.js
@@ -1,23 +1,24 @@
-import AppBar from "@mui/material/AppBar";
+import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import { Fragment, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "../components/Modal";
 import styled from "styled-components";
-
 import apis from "../apis/Apis";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import Button from "@mui/material/Button";
 
 export default function PostPage() {
   const navigate = useNavigate();
   const [imgState, setImgState] = useState([]);
 
-  const [imgFile, setImgFile] = useState([]);
+  const [imgFile, setImgFile] = useState();
 
   const [titleState, setTitleState] = useState("");
   const [contentState, setContentState] = useState("");
   const [value, setValue] = useState("");
-  const [category, setCategory] = useState("카테고리");
+  const [category, setCategory] = useState("카테고리 선택");
   const [open, setOpen] = useState(false);
   const AddImage = (e) => {
     const imgSelectList = e.target.files;
@@ -28,7 +29,6 @@ export default function PostPage() {
     for (let i = 0; i < imgSelectList.length; i++) {
       //전송을 위한 이미지 데이터 추가
       setImgFile(new Array(...imgSelectList));
-
 
       const imgUrl = URL.createObjectURL(imgSelectList[i]);
       imgUploadList.push(imgUrl);
@@ -66,53 +66,67 @@ export default function PostPage() {
 
     const postData = new FormData();
 
-    // console.log(imgFile);
+    console.log(imgFile);
     const dto = {
-      'title': titleState,
-      'content': contentState,
-      'category': category,
-      'postStatus': 'CREATED'
-    }
+      title: titleState,
+      content: contentState,
+      category: category,
+      postStatus: "CREATED",
+    };
 
-    postData.append('requestDto', new Blob([JSON.stringify.apply(dto)], {
-      type: 'multipart/form-data',
-    }));
-    postData.append('imageFileList', imgFile);
+    postData.append(
+      "requestDto",
+      new Blob([JSON.stringify.apply(dto)], {
+        type: "multipart/form-data",
+      })
+    );
+    postData.append("imageFileList", imgFile);
 
     console.log(imgFile);
     console.log(dto);
     //통신
-    apis.writePost(postData).then((response) => {
-      console.log(response);
-    }).catch((error) => {
-      console.log(error);
-    })
-
+    apis
+      .writePost(postData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   const handleDeleteImage = (id) => {
     setImgState(imgState.filter((_, index) => index !== id));
   };
   return (
-    <>
+    <div>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <button
-              style={{ fontSize: "10px" }}
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              되돌아가기
-            </button>
-            <h4>게시글쓰기</h4>
-            <button onClick={ClickHandler}>완료</button>
-          </Toolbar>
-        </AppBar>
+        {/* <Header /> */}
+        <button
+          style={{ fontSize: "10px" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          되돌아가기
+        </button>
+        <h4>게시글쓰기</h4>
+        <button onClick={ClickHandler}>완료</button>
       </Box>
-      <div style={{ border: "1px solid black", height: "100px" }}>
+      <div
+        style={{
+          border: "1px solid gainsboro",
+          height: "100px",
+          borderRight: "0px",
+          borderLeft: "0px",
+
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <label onChange={AddImage}>
-          여기
+          <AddAPhotoIcon
+            style={{ fontSize: "40px", margin: "30px", marginLeft: "15px" }}
+          />
           <input
             type="file"
             multiple
@@ -126,75 +140,137 @@ export default function PostPage() {
             key={id}
             src={image}
             alt={`${image}-${id}`}
-            style={{ width: "60px", height: "60px" }}
+            style={{ width: "55px", height: "70px" }}
             onClick={() => handleDeleteImage(id)}
           />
         ))}
       </div>
-      <div style={{ border: "1px solid black", height: "70px" }}>
-        <input
+      <div>
+        <Service style={{ marginTop: "15px", marginBottom: "5px" }}>
+          글 제목
+        </Service>
+        <InputSt
           type="text"
+          fullWidth
+          label="글 제목"
+          id="title"
           onChange={TitleChange}
           value={titleState}
-          placeholder="제목을 입력해주세요."
+          placeholder="글 제목을 입력해주세요."
         />
       </div>
       <div
         style={{
-          border: "1px solid black",
-          height: "40px",
-          textAlign: "center",
+          border: "1px solid gainsboro",
+          height: "50px",
+          textIndent: "10px",
+          borderTop: "0px",
+          borderRight: "0px",
+          borderLeft: "0px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingRight: "5PX",
         }}
         onClick={OpenModal}
       >
-        <p>{category}</p>
+        <Service style={{ textIndent: "10px" }}>{category}</Service>
+
+        <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
       </div>
-      <div style={{ border: "1px solid black", height: "150px" }}>
-        <input
-          type="text"
+      <div>
+        <Service style={{ marginTop: "15px", marginBottom: "5px" }}>
+          게시글 내용
+        </Service>
+
+        <ContentStyle
+          multiline
+          label="내용"
+          rows={18}
+          placeholder="내용을 입력해주세요."
           onChange={ContentChange}
           value={contentState}
-          placeholder="내용을 입력해주세요."
+          required
         />
       </div>
       <Modal visible={open} closeModal={CloseModal}>
-        <Div data-value="1" onClick={onChangeHandler}>
-          디지털기기
-        </Div>
-        <Div data-value="2" onClick={onChangeHandler}>
-          생활가전
-        </Div>
-        <Div data-value="3" onClick={onChangeHandler}>
-          생활/주방
-        </Div>
-        <Div data-value="4" onClick={onChangeHandler}>
-          여성의류/잡화
-        </Div>
-        <Div data-value="5" onClick={onChangeHandler}>
-          남성의류/잡화
-        </Div>
-        <Div data-value="6" onClick={onChangeHandler}>
-          뷰티/미용
-        </Div>
-        <Div data-value="7" onClick={onChangeHandler}>
-          취미/게임
-        </Div>
-        <Div data-value="8" onClick={onChangeHandler}>
-          도서
-        </Div>
-        <Div data-value="9" onClick={onChangeHandler}>
-          티켓
-        </Div>
-        <button onClick={CloseModal}>돌아가기</button>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <Div data-value="1" onClick={onChangeHandler}>
+            디지털기기
+          </Div>
+          <Div data-value="2" onClick={onChangeHandler}>
+            생활가전
+          </Div>
+          <Div data-value="3" onClick={onChangeHandler}>
+            생활/주방
+          </Div>
+          <Div data-value="4" onClick={onChangeHandler}>
+            여성의류/잡화
+          </Div>
+          <Div data-value="5" onClick={onChangeHandler}>
+            남성의류/잡화
+          </Div>
+          <Div data-value="6" onClick={onChangeHandler}>
+            뷰티/미용
+          </Div>
+          <Div data-value="7" onClick={onChangeHandler}>
+            취미/게임
+          </Div>
+          <Div data-value="8" onClick={onChangeHandler}>
+            도서
+          </Div>
+          <Div data-value="9" onClick={onChangeHandler}>
+            티켓
+          </Div>
+          <Button style={{ marginTop: "20px" }} onClick={CloseModal}>
+            돌아가기
+          </Button>
+        </div>
       </Modal>
-    </>
+    </div>
   );
 }
 const Div = styled.div`
-  align-items: center;
+  margin-top: 10px;
+  border: 0.5px solid gainsboro;
+  height: 40px;
+  border-radius: 10px;
   &:hover {
-    cursor: pointer;
     text-decoration: underline;
-    background-color: 9e90e6;
+    border: 0.5px solid gray;
+    background-color: rgba(0, 0, 0, 0.1);
+    color: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const InputSt = styled.input`
+  border: 1px solid gainsboro;
+  border-right: 0px;
+  border-left: 0px;
+  width: 100%;
+  height: 60px;
+  text-indent: 8px;
+  &:focus {
+    outline: 1px solid gray;
+  }
+`;
+const Service = styled.div`
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  font-size: 14px;
+  text-indent: 10px;
+  font-weight: bold;
+`;
+
+const ContentStyle = styled.textarea`
+  border: 1px solid gainsboro;
+  border-right: 0px;
+  border-left: 0px;
+  width: 100%;
+  text-indent: 8px;
+  font-size: 14px;
+  padding-top: 18px;
+  &:focus {
+    outline: 1px solid gray;
   }
 `;
