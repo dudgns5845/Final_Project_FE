a772c565ca9a3fed0a9a3dafd122460730e17abd
honeyoung <dudgns5845@naver.com>
2022-09-07 18:30:59 +0900

    refactor 리라우터 처리

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import PostPage from "./pages/PostPage";
 import { ThemeProvider } from "styled-components";
@@ -13,6 +13,8 @@ import MyPage from "./pages/MyPage";
 import { useEffect } from "react";
 import Detail from "./pages/DetailPage";
 
+import { getCookie } from "./shared/Cookie";
+
 function App() {
   function setScreenSize() {
     let vh = window.innerHeight * 0.01;
@@ -22,18 +24,29 @@ function App() {
     setScreenSize();
   });
 
+  const cookie = getCookie('accessToken');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (cookie !== undefined) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, [cookie]);
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/searchpage" element={<SearchPage />} />
-          <Route path="*" element={<ErrorPage />} />
           <Route path="/start" element={<Start />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/detail/:postid" element={<Detail />} />
-          <Route path="/postpage" element={<PostPage />} />
           <Route path="/login:id" element={<Login />} />
+          <Route path="/" element={isLoggedIn ? <MainPage /> : <Navigate replace to='/start' />} />
+          <Route path="/searchpage" element={isLoggedIn ? <SearchPage /> : <Navigate replace to='/start' />} />
+          <Route path="/detail/:postid" element={isLoggedIn ? <Detail /> : <Navigate replace to='/start' />} />
+          <Route path="/postpage" element={isLoggedIn ? <PostPage /> : <Navigate replace to='/start' />} />
+          <Route path="/mypage" element={isLoggedIn ? <MyPage /> : <Navigate replace to='/start' />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
