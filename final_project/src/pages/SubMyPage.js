d7a9c3ff47663e1440d69e909c48f1656a80e64a
jho951 <jho951@naver.com>
2022-09-15 14:43:25 +0900

    #1 feat 서브 마이페이지 api 변동

--- final_project/src/pages/SubMyPage.js
+++ final_project/src/pages/SubMyPage.js
@@ -5,18 +5,20 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import apis from "../apis/Apis";
-const temp = [1, 2, 3, 4, 5];
+
 export default function SubMyPage() {
   const navigate = useNavigate();
   const [ttt, setTTT] = useState([]);
 
   useEffect(() => {
     apis
-      .myWritepost("/mypage/posts")
+      .myWritepost()
       .then((response) => {
         console.log(response);
-        setTTT(response.data.data.content);
+        setTTT(response.data.data);
+        console.log(response.data.data);
       })
+
       .catch((error) => {
         console.log(error);
       });
@@ -32,9 +34,8 @@ export default function SubMyPage() {
           }}
         />
       </Header>
-
       <div style={{ marginTop: "5em" }}>
-        {ttt.map((post, idx) => {
+        {ttt?.map((post, idx) => {
           return <Post post={post} key={idx}></Post>;
         })}
       </div>
