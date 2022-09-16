1737144adb17dc514f59d781597a4fee1f8d1614
sangjin <sasangjin@gmail.com>
2022-09-16 21:54:08 +0900

    SearchPage fix
    검색 카테고리 셀렉트 검색어 입력창 mui 적용

--- final_project/src/pages/SearchPage.js
+++ final_project/src/pages/SearchPage.js
@@ -4,6 +4,12 @@ import apis from "../apis/Apis";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
 import Post from "../components/Post";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export default function SearchPage() {
   const navigate = useNavigate();
@@ -40,23 +46,46 @@ export default function SearchPage() {
           }}
         ></ArrowBackIcon>
       </Header>
-      <h1>검색창입니다.</h1>
-      <select onChange={onCategory}>
-        <option value="">---선택---</option>
-        <option value="DEVICE">디지털기기</option>
-        <option value="APPLANCE">생활가전</option>
-        <option value="KITCHEN">생활/주방</option>
-        <option value="WOMEN">여성의류/잡화</option>
-        <option value="MEN">남성의류/잡화</option>
-        <option value="BEAUTY">뷰티/미용</option>
-        <option value="GAME">취미/게임</option>
-        <option value="BOOK">도서</option>
-        <option value="TICKET">티켓</option>
-      </select>
+      <div style={{ marginTop: "18%" }}></div>
+      <FormControl variant="standard" sx={{ width: "40%" }}>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          label="선택"
+          value={search.category}
+          onChange={onCategory}
+          displayEmpty
+        >
+          <MenuItem value="">--선택--</MenuItem>
+          <MenuItem value="DEVICE">디지털기기</MenuItem>
+          <MenuItem value="APPLANCE">생활가전</MenuItem>
+          <MenuItem value="KITCHEN">생활/주방</MenuItem>
+          <MenuItem value="WOMEN">여성의류/잡화</MenuItem>
+          <MenuItem value="MEN">남성의류/잡화</MenuItem>
+          <MenuItem value="BEAUTY">뷰티/미용</MenuItem>
+          <MenuItem value="GAME">취미/게임</MenuItem>
+          <MenuItem value="BOOK">도서</MenuItem>
+          <MenuItem value="TICKET">티켓</MenuItem>
+        </Select>
+      </FormControl>
+      <Button
+        variant="contained"
+        onClick={Click}
+        style={{ marginLeft: "130px" }}
+      >
+        검색하기
+      </Button>
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        placeholder="검색어를 입력해주세요"
+        variant="outlined"
+        onChange={onSearch}
+        // style={{ width: "40%" }}
+      />
+      {/* <input type="text" onChange={onSearch} /> */}
 
-      <input type="text" onChange={onSearch} />
-      <button onClick={Click}>검색하기</button>
-      <div style={{ marginTop: "5em" }}>
+      <div>
         {searchList.map((post, idx) => {
           return <Post post={post} key={idx}></Post>;
         })}
