d1b7c0beb450cc240ff4bb91936e764b2168455d
jho951 <jho951@naver.com>
2022-09-16 17:42:39 +0900

    #1 feat 검색페이지 검색값 포스팅리스트 보여주기 완료

--- final_project/src/pages/SearchPage.js
+++ final_project/src/pages/SearchPage.js
@@ -19,6 +19,7 @@ export default function SearchPage() {
     apis
       .searchList(test)
       .then((response) => {
+        setPostList(response.data.data.content);
         console.log(response);
       })
 
