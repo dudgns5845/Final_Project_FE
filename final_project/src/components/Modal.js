ab932effe7335a66274979b7f1a337022225cf88
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2022-09-20 17:28:08 +0900

    Create Modal.js

---
+++ final_project/src/components/Modal.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+function Modal({ visible, children }) {
+  return (
+    <div className="Modal">
+      <ModalWrap visible={visible}></ModalWrap>
+      <ModalBody visible={visible}>{children}</ModalBody>
+    </div>
+  );
+}
+export default Modal;
+
+const ModalWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: ${(props) => (props.visible ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 99;
+  cursor: pointer;
+`;
+const ModalBody = styled.div`
+  display: ${(props) => (props.visible ? "block" : "none")};
+  position: fixed;
+  top: 54%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+  background-color: white;
+  border-radius: 10px;
+  width: 100vw;
+  height: 90vh;
+  padding-left: 10px;
+  z-index: 100;
+`;
