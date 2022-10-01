0b01faa402b0702df71c7f48e3f006ff5d9da908
jho951 <jho951@naver.com>
2022-10-01 14:00:52 +0900

    #1 feat 알림 카드 css 수정

--- final_project/src/components/AlarmCard.js
+++ final_project/src/components/AlarmCard.js
@@ -5,63 +5,66 @@ import { useState, useEffect } from "react";
 import CreateRoundedIcon from "@mui/icons-material/CreateRounded";
 import { IconButton } from "@mui/material";
 import apis from "../apis/Apis";
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 
 export default function AlarmCard({ alarm }) {
-    const navigate = useNavigate();
-    const [open, setOpen] = useState(alarm.isRead ? false : true)
-    const [alarmStyle, setAlarmStyle] = useState({ backgroundColor: (alarm.isRead ? 'white' : 'orange') })
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(alarm.isRead ? false : true);
+  const [alarmStyle, setAlarmStyle] = useState({
+    backgroundColor: alarm.isRead ? "white" : "orange",
+  });
 
-    //읽음 처리 및 채팅방 이동
-    const clickNotiftcation = () => {
-        apis.readNotification(alarm.notificationId)
-            .then((response) => {
-                console.log(response)
-                setAlarmStyle({ backgroundColor: 'white' });
-                setOpen(false)
-            })
+  //읽음 처리 및 채팅방 이동
+  const clickNotiftcation = () => {
+    apis.readNotification(alarm.notificationId).then((response) => {
+      console.log(response);
+      setAlarmStyle({ backgroundColor: "white" });
+      setOpen(false);
+    });
+  };
 
-    }
-
-    return (
-        <Box sx={{ width: "100vw", display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => navigate(`/detail/${alarm.chatid}`)}>
-            <div style={CardCss}>
-                <div style={{ width: "100px", height: "100px" }}>
-                    <img
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            objectFit: "cover",
-                            borderRadius: "10px",
-                            marginLeft: "10px",
-                        }}
-                        src={alarm.postImageUrl}
-                        alt="null"
-                    />
-                </div>
-
-                <div style={TextCss}>
-                    <h5>{alarm.title}</h5>
-                    <h6>
-
-                    </h6>
-                </div>
-            </div>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "10vh",
+        display: "flex",
+        alignItems: "center",
+        border: "0.5px solid gainsboro",
+        padding: "0 5vw",
+      }}
+      onClick={() => navigate(`/detail/${alarm.chatid}`)}
+    >
+      <div style={CardCss}>
+        <img
+          style={{
+            width: "3.5rem",
+            height: "3.5rem",
+            borderRadius: "10px",
+          }}
+          src={alarm.postImage}
+          alt=""
+        />
+      </div>
+      <div style={TextCss}>
+        <h5>{alarm.content}</h5>
+        <p>d</p>
+      </div>
+    </Box>
+  );
 }
 
 const CardCss = {
-    display: "flex",
-    width: "100%",
-    height: "3vh",
-    padding: "8vh 0",
-    alignItems: "center",
-    borderTop: "0.5px solid gainsboro",
+  display: "flex",
+  borderTop: "0.5px solid gainsboro",
+  borderRadius: "5px",
+  objectFit: "cover",
+  width: "3.5rem",
+  height: "3.5rem",
 };
 
 const TextCss = {
-    paddingLeft: "8vw",
-    width: "100%",
+  paddingLeft: "4vw",
+  width: "80vw",
 };
